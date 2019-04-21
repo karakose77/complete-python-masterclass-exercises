92a34ae92942fab150ec133ebf9c35e6140c6028
Mustafa Karaköse <karakose77@gmail.com>
2019-04-21 20:55:30 +0300

    initial commit

---
+++ mk006-account_decimal.py
@@ -0,0 +1,47 @@
+import decimal
+
+
+class Account(object):
+    """
+    A simple Account class with balance, deposit and withdraw with decimals.
+    """
+    # class constant, accessible without creating an instance.
+    _qb = decimal.Decimal("0.00")
+
+    def __init__(self, name: str, opening_balance: float = 0):
+        self.name = name
+        self._balance = decimal.Decimal(opening_balance).quantize(Account._qb)
+        print(f"Account created for {self.name}. ", end="")
+        self.show_balance()
+
+    def deposit(self, amount: float):
+        decimal_amount = decimal.Decimal(amount).quantize(Account._qb)
+        if decimal_amount > Account._qb:
+            self._balance += decimal_amount
+            print(f"{decimal_amount} deposited.")
+            return self._balance
+
+    def withdraw(self, amount: float):
+        decimal_amount = decimal.Decimal(amount).quantize(Account._qb)
+        if Account._qb < decimal_amount <= self._balance:
+            self._balance -= decimal_amount
+            print(f"{decimal_amount} withdrawn.")
+            return decimal_amount
+        else:
+            print("The amount must be greater than zero"
+                  " and no more than your account balance.")
+            return Account._qb
+
+    def show_balance(self):
+        print(f"Balance on account {self.name} is {self._balance}.")
+
+
+if __name__ == "__main__":
+    mustafa = Account("Mustafa")
+    mustafa.deposit(10.10)
+    mustafa.deposit(0.10)
+    mustafa.deposit(0.10)
+    mustafa.withdraw(0.30)
+    mustafa.withdraw(0.00)
+    mustafa.show_balance()
+    print(mustafa.log)
